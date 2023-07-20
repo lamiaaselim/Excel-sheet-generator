f50dcd15e2be237fd7cce13a4c9e42fd9a859df5
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-21 01:34:42 +0300

    LastModify

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,46 @@
 // // excel-sheet-generator
-
-let rows = document.getElementById('rows');
-let columns = document.getElementById('columns');
-let sheet = document.getElementById('sheet-body');
-let tbl = document.getElementById('table');
-
-const generateTable = () =>{
-    let rowNumber = parseInt(rows.value), colNumber = parseInt(columns.value)
-    // creating all cells
-    for (let i = 0; i < rowNumber; i++) {
-        var tableRow = "";
-        for (let j = 0; j < colNumber; j++) {
-            // Create a <td> element and a text node, make the text
-            tableRow += `<td contenteditable></td>`
-        }
-        sheet.innerHTML += tableRow
+tbl = document.getElementById('table');
+function generateTable() {
+    const numRows = parseInt(document.getElementById("rows").value);
+    const numColumns = parseInt(document.getElementById("columns").value);
+    // condition for valid number of columns and number of rows
+    if (numRows <= 0 || numColumns <= 0 || isNaN(numRows) || isNaN(numColumns)) {
+        alert("Enter a valid number");
+        return;
     }
-    // sets the border attribute of tbl to '2'
-    tbl.setAttribute("border", "2");
+    //  variable to table value
+    let tableContent = "";
+
+    //  create table
+    for (let i = 1; i <= numRows; i++) {
+        tableContent += "<tr>"; // start new row 
+        for (let j = 1; j <= numColumns; j++) {
+            tableContent += "<td contenteditable='true'></td>"; //  start new cell 
+        }
+        tableContent += "</tr>"; // end row  
+    }
+    // append table content
+    const sheetBody = document.querySelector(".sheet-body");
+    sheetBody.innerHTML = tableContent;
+}
+// sets the border attribute of tbl to '2'
+tbl.setAttribute("border", "2");
+
+function ExportToExcel(type, fn, dl) {
+    var elt = document.getElementById('table');
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl ?
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
+        XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
 }
 
 
-
 // // Nice Scrolling
-// $(document).ready(function () {
-// 	"use strict";
-// $("html").niceScroll({
-//     cursorcolor:"#140606",
-// 	cursorwidth:"12px",
-// 	cursorborder:"#E41B17 solid 2px"
-// });
-// });
+$(document).ready(function () {
+	"use strict";
+$("html").niceScroll({
+    cursorcolor:"#00ac47",
+	cursorwidth:"12px",
+	cursorborder:"#black solid 2px"
+});
+});
